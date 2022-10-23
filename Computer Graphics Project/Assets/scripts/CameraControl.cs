970fadaba0275670a8903d0a6c88eae151c21e9e
Vany227 <kyled403@gmail.com>
2022-10-22 19:08:50 -0500

    poop

--- Computer Graphics Project/Assets/scripts/CameraControl.cs
+++ Computer Graphics Project/Assets/scripts/CameraControl.cs
@@ -6,11 +6,19 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
+    private int gridPosX;
+    private int gridPosY;
+    private Camera cam;
+
+
     public Transform screen;
     // Start is called before the first frame update
     void Start()
     {
-        //this.transform.position = screen.position;         
+        gridPosX = 0;
+        gridPosY = 0;
+        cam = GetComponent<Camera>();
+        
     }
 
     // Update is called once per frame
--- Computer Graphics Project/Assets/scripts/GameController.cs
+++ Computer Graphics Project/Assets/scripts/GameController.cs
@@ -7,13 +7,17 @@ public class GameController : MonoBehaviour
     
     private Vector3Int startingPOS = new Vector3Int(0, 0, 0);
     private Grid grid;
+    private Mode mode;
     public enum Type { TWOXONE, TWOXTWO, THREEXTHREE };
+    public enum Mode { TWOD, THREED};
     public List<Transform> screens = new List<Transform>();
     public Type m_type;
     public Transform Camera;
     // Start is called before the first frame update
     void Start()
     {
+        mode = Mode.TWOD;
+
         grid = this.GetComponent<Grid>();
         Vector3 Startposition = grid.CellToWorld(startingPOS);
         Startposition.Set(Startposition.x + 16, Startposition.y + 16, -30);
@@ -24,6 +28,7 @@ public class GameController : MonoBehaviour
         Startposition.Set(Startposition.x - 16, Startposition.y - 16, currentScreen.position.z);
         currentScreen.position = Startposition;
 
+        
         /*
         switch (m_type){
             case Type.TWOXTWO:
