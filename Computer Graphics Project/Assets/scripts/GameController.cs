c8ad0c273d7113914c68bd5aed84469470d3092e
Vany227 <kyled403@gmail.com>
2022-10-27 13:39:14 -0500

    stuff

--- Computer Graphics Project/Assets/scripts/GameController.cs
+++ Computer Graphics Project/Assets/scripts/GameController.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.VisualScripting;
 
 public class GameController : MonoBehaviour
 {
-    
+    private Vector3 Startposition;
     private Vector3Int startingPOS = new Vector3Int(0, 0, 0);
     private Grid grid;
     private Mode mode;
@@ -17,25 +18,29 @@ public class GameController : MonoBehaviour
     void Start()
     {
         mode = Mode.TWOD;
-
         grid = this.GetComponent<Grid>();
-        Vector3 Startposition = grid.CellToWorld(startingPOS);
-        Startposition.Set(Startposition.x + 16, Startposition.y + 16, -30);
-        Debug.Log(Startposition);
-        Camera.position = Startposition;
-
-        Transform currentScreen = screens[0];
-        Startposition.Set(Startposition.x - 16, Startposition.y - 16, currentScreen.position.z);
-        currentScreen.position = Startposition;
+        
 
         
-        /*
+        
         switch (m_type){
             case Type.TWOXTWO:
+                for (int i = 0; i < screens.Count; i++)
+                {
+                    Transform currentScreen = screens[i];
+                    Startposition = grid.CellToWorld(new Vector3Int(currentScreen.GetComponent<ScreenController>().GridX, currentScreen.GetComponent<ScreenController>().GridY, -30));
+                    Startposition.Set(Startposition.x, Startposition.y, currentScreen.position.z);
+                    currentScreen.position = Startposition;
 
+                    if (currentScreen.GetComponent<ScreenController>().startingScreen)
+                    {
+                        Startposition.Set(Startposition.x + 16, Startposition.y + 16, -30);
+                        Camera.position = Startposition;
+                    }
+                }
                 break;
         }
-        */
+        
     }
 
     // Update is called once per frame
