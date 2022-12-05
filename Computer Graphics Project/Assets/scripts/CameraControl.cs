c54189c3f400f72e09f2ac2d6e62bcc6abd50e06
Vany227 <kyled403@gmail.com>
2022-12-05 03:08:20 -0600

    tesing some goofy stuff

--- Computer Graphics Project/Assets/scripts/CameraControl.cs
+++ Computer Graphics Project/Assets/scripts/CameraControl.cs
@@ -8,24 +8,38 @@ public class CameraControl : MonoBehaviour
 {
     private int gridPosX;
     private int gridPosY;
+    private Matrix4x4 orthographic, perspective;
+    private bool orthoOn;
     private Camera cam;
 
     public Grid grid;
     public Transform Character;
     public Transform screen;
+    private MatrixBlender blender;
     // Start is called before the first frame update
     void Start()
     {
         gridPosX = 0;
         gridPosY = 0;
         cam = GetComponent<Camera>();
-        
+        orthographic = Matrix4x4.Ortho(-16, 16, -16, 16, 0, 100);
+        perspective = Matrix4x4.Perspective(101, (float)Screen.width / (float)Screen.height, 0, 100);
+        cam.projectionMatrix = orthographic;
+        orthoOn = true;
+        blender = (MatrixBlender)GetComponent(typeof(MatrixBlender));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            orthoOn = !orthoOn;
+            if (orthoOn)
+                blender.BlendToMatrix(orthographic, 1f);
+            else
+                blender.BlendToMatrix(perspective, 1f);
+        }
     }
 
     public void UpdateScreen()
